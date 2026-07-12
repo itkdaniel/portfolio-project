35653dbec5474cd6ac67e82f17a72cab4d153cd5
Daniel Truong <30559196+itkdaniel@users.noreply.github.com>
2026-07-11 22:28:09 -0700

    feat(db): schema barrel export

---
+++ lib/db/src/schema/index.ts
@@ -0,0 +1,7 @@
+export * from "./users";
+export * from "./profiles";
+export * from "./dataSources";
+export * from "./scrapeJobs";
+export * from "./featureSets";
+export * from "./trainingRuns";
+export * from "./testRuns";
